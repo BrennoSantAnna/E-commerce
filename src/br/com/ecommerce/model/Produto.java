0bcf27be7efc1da6520dc5e62ea48224f96035eb
Brenno Sant'Anna <brennopaiva1@outlook.com>
2025-11-10 22:43:56 -0300

    Criação da classe Produto

---
+++ src/br/com/ecommerce/model/Produto.java
@@ -0,0 +1,61 @@
+package br.com.ecommerce.model;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private String categoria;
+    private int quantidade;
+    private double preco;
+    private double avaliacao;
+
+    public Produto(int codigo, String nome, String categoria, int quantidade, double preco, double avaliacao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.avaliacao = avaliacao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome() {
+        this.nome = nome;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria() {
+        this.categoria = categoria;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade() {
+        this.quantidade = quantidade;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco() {
+        this.preco = preco;
+    }
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+    public void setAvaliacao() {
+        this.avaliacao = avaliacao;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Produto [Código: %d, Nome: '%s', Categoria: '%s', Qtd: %d, Preço: R$%.2f, Avaliação: %.1f]",
+                codigo, nome, categoria, quantidade, preco, avaliacao
+        );
+    }
+}
