7e310ee7b9ce29bd39252dfcefa5fe2c68465004
Brenno Sant'Anna <brennopaiva1@outlook.com>
2025-11-12 22:40:48 -0300

    Implementação dos métodos de busca por código/categoria/nome e ordenação com Bubble sort

--- src/br/com/ecommerce/data/Estoque.java
+++ src/br/com/ecommerce/data/Estoque.java
@@ -1,5 +1,6 @@
 package br.com.ecommerce.data;
 
+import br.com.ecommerce.algoritmos.ordenacao.BubbleSort;
 import br.com.ecommerce.model.Produto;
 import java.util.ArrayList;
 
@@ -31,6 +32,53 @@ public class Estoque {
         } else {
             System.out.println("Produto com código " + codigoParaRemover + " não encontrado no estoque.");
         }
+    }
+
+    public Produto buscarProdutoPorCodigo(int codigo) {
+        for (Produto p : this.listaDeProdutos) {
+            if (p.getCodigo() == codigo) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Produto> buscarPorCategoria(String categoriaBuscada) {
+        ArrayList<Produto> listaDeResultados = new ArrayList<>();
+
+        for (Produto p : this.listaDeProdutos) {
+            if (p.getCategoria().equals(categoriaBuscada)) {
+                listaDeResultados.add(p);
+            }
+        }
+        return listaDeResultados;
+    }
+
+    public ArrayList<Produto> buscarPorNome(String nomeBuscado) {
+        ArrayList<Produto> listaDeResultados = new ArrayList<>();
+
+        for (Produto p : this.listaDeProdutos) {
+            if (p.getNome().toLowerCase().contains(nomeBuscado.toLowerCase())) {
+                listaDeResultados.add(p);
+            }
+        }
+
+        return listaDeResultados;
+    }
+
+    public void listarProdutosOrdenadosPorPreco() {
+        if (this.listaDeProdutos.isEmpty()) {
+            System.out.println("O estoque está vazio. Nada para ordenar.");
+            return;
+        }
+        ArrayList<Produto> listaCopiada = new ArrayList<>(this.listaDeProdutos);
+        BubbleSort.ordenarPorPreco(listaCopiada);
+
+        System.out.println("\n--- Lista de Produtos (Ordenada por preço) ---");
+        for (Produto p : listaCopiada) {
+            System.out.println(p);
+        }
+        System.out.println("------------------------------------------------------------------------------------------------");
     }
 
     public ArrayList<Produto> getListaDeProdutos() {
