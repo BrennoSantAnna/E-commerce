e184b271deb8039711a1ce8cd9f22c604ec69715
Brenno <brennopaiva1@outlook.com>
2025-11-13 22:26:57 -0300

    Editando arquivo pelo GitHub
    Added a comment for testing the editing of the Main file.

--- src/br/com/ecommerce/main/Main.java
+++ src/br/com/ecommerce/main/Main.java
@@ -30,5 +30,7 @@ public class Main {
 
         System.out.println("\n*** Lista original (verificando integridade) ***");
         estoque.listarTodosOsProdutos();
+
+        /* Testando edição do arquivo Main*/
     }
 }
