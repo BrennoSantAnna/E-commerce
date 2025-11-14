ee2fa354187d9dfbfaa8f9700ff892b753e046e8
Brenno Sant'Anna <brennopaiva1@outlook.com>
2025-11-13 23:19:06 -0300

    Implementação do algoritmo de ordenação Selection Sort, organizando a lista por quantidade

---
+++ src/br/com/ecommerce/algoritmos/ordenacao/SelectionSort.java
@@ -0,0 +1,30 @@
+package br.com.ecommerce.algoritmos.ordenacao;
+
+import br.com.ecommerce.model.Produto;
+import java.util.ArrayList;
+
+public class SelectionSort {
+    public static void ordenarPorQuantidade(ArrayList<Produto> lista) {
+        if (lista == null || lista.size() < 2) {
+            return;
+        }
+
+        int n = lista.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            int indiceDoMenor = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (lista.get(j).getQuantidade() < lista.get(indiceDoMenor).getQuantidade()) {
+                    indiceDoMenor = j;
+                }
+            }
+
+            if (indiceDoMenor != 1) {
+                Produto temp = lista.get(indiceDoMenor);
+                lista.set(indiceDoMenor, lista.get(i));
+                lista.set(i, temp);
+            }
+        }
+    }
+}
