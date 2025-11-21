e2d167801a48949fad6f35b5d4d01a274dccc03b
Brenno Sant'Anna <brennopaiva1@outlook.com>
2025-11-20 22:36:39 -0300

    Junção dos 'imports'

--- src/br/com/ecommerce/main/Main.java
+++ src/br/com/ecommerce/main/Main.java
@@ -3,7 +3,6 @@ package br.com.ecommerce.main;
 import br.com.ecommerce.model.Produto;
 import br.com.ecommerce.data.Estoque;
 import br.com.ecommerce.view.MenuConsole;
-
 import java.util.ArrayList;
 
 public class Main {
