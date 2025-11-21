41164d15f2ddf71d45a405e0f6cb183cf2093acd
Brenno Sant'Anna <brennopaiva1@outlook.com>
2025-11-20 22:19:39 -0300

    Refatorando código: redução de condicional e adição de documentação com JavaDoc

--- src/br/com/ecommerce/algoritmos/ordenacao/BubbleSort.java
+++ src/br/com/ecommerce/algoritmos/ordenacao/BubbleSort.java
@@ -5,19 +5,20 @@ import java.util.ArrayList;
 
 public class BubbleSort {
     public static void ordenarPorPreco(ArrayList<Produto> lista) {
-        if (lista == null || lista.size() < 2) {
-            return;
-        }
+        if (lista == null || lista.size() < 2) return;
 
         int n = lista.size();
 
+        // Laço externo: define quantas passadas faremos na lista
         for (int i = 0; i < n - 1; i++) {
             boolean trocou = false;
 
+            // Laço interno: compara elementos adjacentes
             for (int j = 0; j < n - 1 - i; j++) {
                 Produto prod1 = lista.get(j);
                 Produto prod2 = lista.get(j + 1);
 
+                // Critério da ordenação: Preço
                 if (prod1.getPreco() > prod2.getPreco()) {
                     lista.set(j, prod2);
                     lista.set(j + 1, prod1);
@@ -25,9 +26,8 @@ public class BubbleSort {
                 }
             }
 
-            if (!trocou) {
-                break;
-            }
+            // Se não houver nenhuma troca nesta passada, a lista ja está ordenada
+            if (!trocou) break;
         }
     }
 }
